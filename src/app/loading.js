07c420e2462c1b6bcf7e96de1bd7561b920becde
AnthonyGKruger <60028700+AnthonyGKruger@users.noreply.github.com>
2023-07-21 20:12:12 +0200

    working on dark mode feature

--- src/app/loading.js
+++ src/app/loading.js
@@ -60,7 +60,7 @@ export default () => {
           />
           <path
             d="M12 22C14.6522 22 17.1957 20.9464 19.0711 19.0711C20.9464 17.1957 22 14.6522 22 12C22 9.34784 20.9464 6.8043 19.0711 4.92893C17.1957 3.05357 14.6522 2 12 2"
-            className="stroke-primary-blue"
+            className="stroke-primary-blue dark:stroke-primary-gold"
             strokeWidth="4"
           />
         </svg>
