10e1b55b7b9b9111837dc5a85abd1cb59add041c
AnthonyGKruger <60028700+AnthonyGKruger@users.noreply.github.com>
2023-09-08 18:00:52 +0200

    Update database config

--- src/app/layout.js
+++ src/app/layout.js
@@ -34,31 +34,11 @@ const Layout = ({ children }) => {
             });
           `}
       </Script>
-      <Script id="google-analytics-config" strategy="lazyOnload">
-        {`
-          function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer',${process.env.NEXT_PUBLIC_GTM});
-        `}
-      </Script>
 
       {/* Define the body element */}
       <body
         className={`${roboto.className} bg-white dark:bg-black dark:text-zinc-200 text-alternative-blue relative`}
       >
-        <Script id={`gtm-script`}>
-          <noscript>
-            <iframe
-              src="https://www.googletagmanager.com/ns.html?id=GTM-WRTN27BM"
-              height="0"
-              width="0"
-              style="display:none;visibility:hidden"
-            ></iframe>
-          </noscript>
-        </Script>
-
         {/* Redux store provider */}
         <ReduxProviderHelper>
           {/* Theme provider for next-themes */}
