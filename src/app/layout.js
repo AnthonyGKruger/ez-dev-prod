2909adcaf9417ef44171fadba0d4347dad69e2f9
AnthonyGKruger <60028700+AnthonyGKruger@users.noreply.github.com>
2023-09-08 17:29:51 +0200

    implimenting gtm

--- src/app/layout.js
+++ src/app/layout.js
@@ -18,22 +18,22 @@ const Layout = ({ children }) => {
   return (
     <html lang="en">
       {/* Load Google Analytics script asynchronously */}
-      {/*<Script*/}
-      {/*  strategy="lazyOnload"*/}
-      {/*  src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}*/}
-      {/*/>*/}
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
 
       {/*/!*Define the Google Analytics configuration *!/*/}
-      {/*<Script id="google-analytics-config" strategy="lazyOnload">*/}
-      {/*  {`*/}
-      {/*      window.dataLayer = window.dataLayer || [];*/}
-      {/*      function gtag(){dataLayer.push(arguments);}*/}
-      {/*      gtag('js', new Date());*/}
-      {/*      gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {*/}
-      {/*        page_path: window.location.pathname,*/}
-      {/*      });*/}
-      {/*    `}*/}
-      {/*</Script>*/}
+      <Script id="google-analytics-config" strategy="lazyOnload">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `}
+      </Script>
       <Script id="google-analytics-config" strategy="lazyOnload">
         {`
           function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
