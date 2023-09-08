aa7696c4897fceed8b05910df864ce95cc919e8a
AnthonyGKruger <60028700+AnthonyGKruger@users.noreply.github.com>
2023-09-08 17:41:23 +0200

    implimenting gtm

--- src/app/layout.js
+++ src/app/layout.js
@@ -25,23 +25,23 @@ const Layout = ({ children }) => {
 
       {/*/!*Define the Google Analytics configuration *!/*/}
       <Script id="google-analytics-config" strategy="lazyOnload">
-        {`
+        {
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
             gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
               page_path: window.location.pathname,
             });
-          `}
+          }
       </Script>
       <Script id="google-analytics-config" strategy="lazyOnload">
-        {`
+        {
           function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
           })(window,document,'script','dataLayer',${process.env.NEXT_PUBLIC_GTM});
-        `}
+        }
       </Script>
 
       {/* Define the body element */}
