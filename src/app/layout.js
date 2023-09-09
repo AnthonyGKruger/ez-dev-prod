37a94e49f6295d1cdfbc591c00ab368b312f58ff
AnthonyGKruger <60028700+AnthonyGKruger@users.noreply.github.com>
2023-09-09 06:24:19 +0200

    updating tag manager

--- src/app/layout.js
+++ src/app/layout.js
@@ -18,27 +18,44 @@ const Layout = ({ children }) => {
   return (
     <html lang="en">
       {/* Load Google Analytics script asynchronously */}
-      <Script
-        strategy="lazyOnload"
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-      />
+      {/*<Script*/}
+      {/*  strategy="lazyOnload"*/}
+      {/*  src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}*/}
+      {/*/>*/}
 
-      {/*/!*Define the Google Analytics configuration *!/*/}
-      <Script id="google-analytics-config" strategy="lazyOnload">
-        {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-              page_path: window.location.pathname,
-            });
-          `}
+      {/*/!*/!*Define the Google Analytics configuration *!/*!/*/}
+      {/*<Script id="google-analytics-config" strategy="lazyOnload">*/}
+      {/*  {`*/}
+      {/*      window.dataLayer = window.dataLayer || [];*/}
+      {/*      function gtag(){dataLayer.push(arguments);}*/}
+      {/*      gtag('js', new Date());*/}
+      {/*      gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {*/}
+      {/*        page_path: window.location.pathname,*/}
+      {/*      });*/}
+      {/*    `}*/}
+      {/*</Script>*/}
+
+      <Script id={"google-tag-manager"}>
+        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-WRTN27BM');`}
       </Script>
 
       {/* Define the body element */}
       <body
         className={`${roboto.className} bg-white dark:bg-black dark:text-zinc-200 text-alternative-blue relative`}
       >
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WRTN27BM"
+            height="0"
+            width="0"
+            style="display:none;visibility:hidden"
+          ></iframe>
+        </noscript>
+
         {/* Redux store provider */}
         <ReduxProviderHelper>
           {/* Theme provider for next-themes */}
