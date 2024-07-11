32b390a64b5503657a9d8c972995aa8b84b8f1e8
AnthonyGKruger <antman.kruger@gmail.com>
2024-07-11 04:32:28 +0200

    removing some uncompleted projects from portfolio until I find time to complete them, focus is on Saas solution for now.

--- src/app/api/portfolio/route.js
+++ src/app/api/portfolio/route.js
@@ -34,32 +34,32 @@ export const GET = async (request) => {
         "https://github.com/AnthonyGKruger/Mumbi-Trust-Administrators",
     },
 
-    {
-      id: "p5",
-      title: "Star Wars E-commerce App",
-      description:
-        "Ecommerce app built NextJS and TailWind CSS - (still under development.)",
-      link: "https://swapi-ecommerce.vercel.app/",
-      image: "/media/logos/star-wars-logo.svg",
-      sourceCode: "https://github.com/AnthonyGKruger/swapi-ecommerce",
-    },
-    {
-      id: "p6",
-      title: "Fan Catalogue",
-      description: "Fan Catalogue built using Next.js and Tailwind CSS.",
-      link: "https://tlwfans.vercel.app",
-      image: "/media/logos/TLW-logo.jpg",
-      sourceCode: "https://github.com/AnthonyGKruger/TLWfans",
-    },
-    {
-      id: "p4",
-      title: "Smarten-Up Recruitment Assessment",
-      description: "Figma mockup assessment using React.js and Tailwind CSS.",
-      link: "https://smarten-up.vercel.app/",
-      image:
-        "https://smartenup-figma-test.s3.eu-central-1.amazonaws.com/assets/icon.png",
-      sourceCode: "https://github.com/AnthonyGKruger/smarten",
-    },
+    // {
+    //   id: "p5",
+    //   title: "Star Wars E-commerce App",
+    //   description:
+    //     "Ecommerce app built NextJS and TailWind CSS - (still under development.)",
+    //   link: "https://swapi-ecommerce.vercel.app/",
+    //   image: "/media/logos/star-wars-logo.svg",
+    //   sourceCode: "https://github.com/AnthonyGKruger/swapi-ecommerce",
+    // },
+    // {
+    //   id: "p6",
+    //   title: "Fan Catalogue",
+    //   description: "Fan Catalogue built using Next.js and Tailwind CSS.",
+    //   link: "https://tlwfans.vercel.app",
+    //   image: "/media/logos/TLW-logo.jpg",
+    //   sourceCode: "https://github.com/AnthonyGKruger/TLWfans",
+    // },
+    // {
+    //   id: "p4",
+    //   title: "Smarten-Up Recruitment Assessment",
+    //   description: "Figma mockup assessment using React.js and Tailwind CSS.",
+    //   link: "https://smarten-up.vercel.app/",
+    //   image:
+    //     "https://smartenup-figma-test.s3.eu-central-1.amazonaws.com/assets/icon.png",
+    //   sourceCode: "https://github.com/AnthonyGKruger/smarten",
+    // },
   ];
   // Return a JSON response with the project data and status 200
   return new Response(JSON.stringify(responseData), {
